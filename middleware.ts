725cd7163dda9fff06469b2ed8a694c439f373b5
Arnaud <arnaud3488@gmail.com>
2024-12-20 16:41:11 +0700

    fix middleware to protect all private routes

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const websiteUrl = request.cookies.get("websiteUrl");
-  const eventId = request.cookies.get("eventId");
+  const websiteUrl = request.cookies.get("websiteUrl")?.value;
+  const eventId = request.cookies.get("eventId")?.value;
 
   if (!websiteUrl || !eventId) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/dashboard",
+  matcher: ["/dashboard", "/dashboard/:path*"],
 };
